c8e6ba065bd2284ebd2886552356e208007dcbc5
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2016-12-22 18:43:12 +0100

    App lauffähig mit Button; .gitignore angepasst

--- arkecho-app/arkecho-app/Resources/Resource.Designer.cs
+++ arkecho-app/arkecho-app/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace arkecho_app
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int lblHelloWorld = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int pbClickMe = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
