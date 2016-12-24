5e7cd22a8122fcebfb59acecc7cc71c73cb65cd6
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2016-12-24 12:47:39 +0100

    Implemented WebSocket Connection to Player

--- arkecho-app/arkecho-app/Source/MainActivity.cs
+++ arkecho-app/arkecho-app/Source/MainActivity.cs
@@ -1,31 +1,78 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
-using System.Net.WebSockets;
+using System.Threading.Tasks;
 
 namespace arkecho_app
 {
     [Activity(Label = "ArkEcho", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Websockets.IWebSocketConnection connection;
+        private bool Failed;
+        private bool Echo;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
-
             FindViewById<Button>(Resource.Id.pbClickMe).Click += onPbClickMeClick;
+            
+            Websockets.Droid.WebsocketConnection.Link();
         }
 
-        private void onPbClickMeClick(object sender, System.EventArgs e)
+        private async void onPbClickMeClick(object sender, System.EventArgs e)
         {
-            ClientWebSocket socket = new ClientWebSocket();
-            System.Threading.CancellationToken token;
-            System.Uri uri = new System.Uri("ws://192.168.178.20:1000");
+            connection = Websockets.WebSocketFactory.Create();
+            connection.OnOpened += Connection_OnOpened;
+            connection.OnMessage += Connection_OnMessage;
+            connection.OnError += Connection_OnError;
+            connection.OnLog += Connection_OnLog;
+            
+            Echo = Failed = false;
+            Timeout();
+            
+            connection.Open("ws://192.168.178.20:1000");
 
-            socket.ConnectAsync(uri, token);
-            //TextView lblHelloWorld = FindViewById<TextView>(Resource.Id.lblHelloWorld);
-            //lblHelloWorld.Text = "Test";
+            while (!connection.IsOpen && !Failed)
+            {
+                await Task.Delay(10);
+            }
+
+            if (!connection.IsOpen) return;
+            
+            connection.Send("Hello World");
+
+            while (!Echo && !Failed)
+            {
+                await Task.Delay(10);
+            }
+
+            if (!Echo) return;
+        }
+
+        async void Timeout()
+        {
+            await Task.Delay(120000);
+            Failed = true;
+        }
+
+        private void Connection_OnOpened()
+        {
+        }
+
+        private void Connection_OnMessage(string obj)
+        {
+        }
+
+        private void Connection_OnError(string obj)
+        {
+            Failed = true;
+        }
+
+        private void Connection_OnLog(string obj)
+        {
         }
     }
 }
