85ce44d5360732af1b100de8546233ace325f640
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2017-02-15 16:36:36 +0100

    Implemented simple Settings Control with ListWidget and changing Page of StackWidget

--- arkecho_player/arkecho_player/Source/ArkEchoSettingsView.cpp
+++ arkecho_player/arkecho_player/Source/ArkEchoSettingsView.cpp
@@ -1,6 +1,14 @@
 #include "ArkEchoSettingsView.h"
 #include "ArkEchoSettingsModel.h"
 
+const QString DIALOGTITLE = "Einstellungen";
+
+enum SWSETTINGSPAGEINDEX
+{
+    PATH = 0,
+    TEST = 1
+};
+
 ArkEchoSettingsView::ArkEchoSettingsView(QWidget *parent)
     : QDialog(parent)
     ,ui_(0)
@@ -8,8 +16,11 @@ ArkEchoSettingsView::ArkEchoSettingsView(QWidget *parent)
 {
     ui_ = new Ui::ArkEchoSettingsView();
     ui_->setupUi(this);
+    connect(ui_->lwSettings, SIGNAL(itemPressed(QListWidgetItem*)), this, SLOT(onLwSettingsItemPressed(QListWidgetItem*)));
 
     model_ = new ArkEchoSettingsModel();
+
+    initUi();
 }
 
 ArkEchoSettingsView::~ArkEchoSettingsView()
@@ -18,3 +29,26 @@ ArkEchoSettingsView::~ArkEchoSettingsView()
     delete ui_;
 }
 
+void ArkEchoSettingsView::initUi()
+{
+    this->setWindowTitle(DIALOGTITLE);
+
+    // ListWidget initilisieren
+    QListWidgetItem* item0 = new QListWidgetItem("Dateipfade anpassen", ui_->lwSettings, SWSETTINGSPAGEINDEX::PATH);
+    QListWidgetItem* item1 = new QListWidgetItem("Test", ui_->lwSettings, SWSETTINGSPAGEINDEX::TEST);
+
+    // Set selected Page & Item
+    ui_->lwSettings->setItemSelected(item0, true);
+    ui_->swSettings->setCurrentIndex(0);
+}
+
+void ArkEchoSettingsView::setSwSettingsPageIndex(int index)
+{
+    ui_->swSettings->setCurrentIndex(index);
+}
+
+void ArkEchoSettingsView::onLwSettingsItemPressed(QListWidgetItem* item)
+{
+    if (!item) return;
+    setSwSettingsPageIndex(item->type());
+}
--- arkecho_player/arkecho_player/Source/ArkEchoSettingsView.h
+++ arkecho_player/arkecho_player/Source/ArkEchoSettingsView.h
@@ -12,10 +12,16 @@ class ArkEchoSettingsView : public QDialog
 
     Ui::ArkEchoSettingsView *ui_;
     ArkEchoSettingsModel* model_;
+    
+    void initUi();
+    void setSwSettingsPageIndex(int index);
 
 public:
     ArkEchoSettingsView(QWidget *parent = 0);
     ~ArkEchoSettingsView();
+
+private slots:
+    void onLwSettingsItemPressed(QListWidgetItem* item);
 };
 
 #endif // ARKECHOSETTINGSVIEW_H
